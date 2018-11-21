703ad87601d9b297816a209c4d5397143b0d31d0
Charilaos Skandylas <skandylas@teknik.io>
2018-11-21 17:13:23 +0100

    Create babysteps.py
    BabySteps(1)

---
+++ babysteps.py
@@ -0,0 +1,19 @@
+#write a program that sums two numbers and prints the result
+
+#write a program that subtracts a number from another and prints the result
+
+#write a program that multiplies two numbers and prints the result
+
+#write a program that divides two numbers and prints the result
+
+#write a program that adds two numbers if at least one of them is greater than 10
+
+#write a program that adds two numbers if at least one of them is greater than 10, otherwise it subtracts one from the other
+
+#write a program that prints your name 3 times using string multiplication
+
+#write a program that sums all numbers from a to b
+
+#write a program tha reads two variables(numbers) from the user and prints their sum,difference,product,remainder,divisor
+
+#write a program that reads a name from the user and if the name is bob prints "hi bob" otherwise it prints hi name for each letter in the name's length  
