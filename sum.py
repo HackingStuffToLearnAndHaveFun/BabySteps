bfdf0ecbeb4e903357f6563dc8fc47b2419067fb
Alexator25 <45237464+Alexator25@users.noreply.github.com>
2018-11-23 19:22:02 +0200

    Update sum.py

--- sum.py
+++ sum.py
@@ -3,3 +3,19 @@ sum=0
 for i in range(0,100):
   sum=sum+i
 print (sum)
+'''
+a8roisths 2 ari8mwn
+den uparxei sthn python mia aplh entolh diavase h opoia na stamata thn ektelesh tou programmatos kai na emfanizei sthn o8onh p trexei enan kersora gia na dwsei o xrhsths times?
+prepei na kaneis auto to prama apo katw?
+(den exw teleiwsei akoma kai ta 56 tutorials)
+'''
+
+
+
+def mthfk(x, y):
+  sum = x + y
+  print(sum)
+
+
+mthfk(3,4)
+
