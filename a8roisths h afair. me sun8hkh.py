6f783ae118b1a6de8fc1ccf9578a29ea46aed1ca
Alexator25 <45237464+Alexator25@users.noreply.github.com>
2018-11-23 23:33:32 +0200

    Update a8roisths h afair. me sun8hkh.py

--- a8roisths h afair. me sun8hkh.py
+++ a8roisths h afair. me sun8hkh.py
@@ -1,12 +1,6 @@
-'''
-kapou kanw kati la8os alla den mporw na to entopisw...
-'''
-
-
-
-x = input("give me a number")
-y = input("and another one")
-if x>10 or y>10 :
+x = int(input("give me a number"))
+y = int(input("and another one"))
+if x > 10 or y > 10 :
     print (x+y)
 else:
     print (x-y)
