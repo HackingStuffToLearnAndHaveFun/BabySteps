0a3e9676dd40ce77cc2c6a33d19a9e5572449d88
Alexator25 <45237464+Alexator25@users.noreply.github.com>
2018-11-23 22:31:42 +0200

    Create a8roisths h afair. me sun8hkh.py

---
+++ a8roisths h afair. me sun8hkh.py
@@ -0,0 +1,12 @@
+'''
+kapou kanw kati la8os alla den mporw na to entopisw...
+'''
+
+
+
+x = input("give me a number")
+y = input("and another one")
+if x>10 or y>10 :
+    print (x+y)
+else:
+    print (x-y)
