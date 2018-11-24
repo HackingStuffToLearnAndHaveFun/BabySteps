f498d694f392607e095430ee947844deded1edea
Charilaos Skandylas <skandylas@teknik.io>
2018-11-24 22:00:49 +0100

    BabySteps2

--- BabySteps2/BabySteps2.py
+++ BabySteps2/BabySteps2.py
@@ -1,9 +1,9 @@
-#
+#Write a program that read the user's details(First Name,Surname, Phonenumber,E-mail) and stores them in a file
 
-#
+#Write a program that reads the user's details from a file (Id,First Name,Surname, Phonenumber,E-mail) and prints them to the users
 
-#
+#Write a program that reads the users details(Id,First Name,Surname, Phonenumber,E-mail) from the keyboard and stores them in a file
 
-#
+#Write a program that reads the users details(Id,First Name,Surname, Phonenumber,E-mail) from the keyboard and stores them in a file
 
-#
+#Write a program that reads the users details(Id,First Name,Surname, Phonenumber,E-mail) from a file and then asks the user for an Id, then prints that user's details
