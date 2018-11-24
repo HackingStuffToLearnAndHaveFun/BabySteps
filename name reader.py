3e9180d6b4275678a7fea2ed3b21af29533bf64c
Alexator25 <45237464+Alexator25@users.noreply.github.com>
2018-11-24 17:41:24 +0200

    Create name reader.py

---
+++ name reader.py
@@ -0,0 +1,10 @@
+n = input("whats your name?")
+if n = "Bob" :
+    print ("hi Bob")
+else:
+    print ((len(n))* n)
+
+
+'''
+afou to input diavazei xarakthres gt sto if n = "b0b" m petaei invalid syntax
+'''
