c5dc33eae526ea2e574a0b274133c172a488ddc8
Alexator25 <45237464+Alexator25@users.noreply.github.com>
2018-11-26 15:29:44 +0200

    Create test.py

---
+++ BabySteps2/test.py
@@ -0,0 +1,6 @@
+with open ("babysteps1.txt", "r") as f:
+    f_contents = f.read ()
+    print (f_contents)
+
+'''
+den einai kapoia ask prospa8w na dw pws douleuei h klhsh fakelou , ti paei strava?'''
