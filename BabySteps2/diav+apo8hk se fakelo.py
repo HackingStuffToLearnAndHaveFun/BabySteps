c775c157425ed7f857607da31e65151547066666
Alexator25 <45237464+Alexator25@users.noreply.github.com>
2018-12-05 19:34:41 +0200

    Create diav+apo8hk se fakelo.py

---
+++ BabySteps2/diav+apo8hk se fakelo.py
@@ -0,0 +1,8 @@
+fn = input ("what's your first name?")
+sn = input ("what's your surname?")
+em = input ("what's your email?")
+tn = input (what's your telephone number?')
+filename = "motherfucker's shit.txt"
+file = open(filename , "w")
+  f.write (fn , sn , em , tn)
+file.close()
