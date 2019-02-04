77c824c54a8d40ea23e588445fc6cdb7d9d01fcc
Charilaos Skandylas <charilaos.skandylas@lnu.se>
2019-02-04 18:44:18 +0100

    BabySteps3

---
+++ BabySteps3/BabySteps3.py
@@ -0,0 +1,20 @@
+#Write a program that ask the user for 5 types of food and their price, stores the food and price in a dictionary and then returns the most and least expensive item in the dictionary
+
+#Write a program that serves as a simple song lookup list: it reads a file whose format is:
+#song:link\newline song:link\newline... stores the contents in a dictionary and allows the user
+#to search for a song , if the song is found the link is returned otherwise it allows the user
+#to add the link to the dictionary when the user enters None then the program saves the dictionary
+#to the file and then exits
+
+#Write a program that corresponds to a simple hotel booking and room lookup service:
+#On startup the program displays the following menu:
+#1) Load Hotel File
+#2) Add Reservation(person,start_date,end_date,room)
+#3) Look up reservations by person X
+#4) Look up reservations by room X
+#5) Find if room X is available on date Y, X and Y are given by the user
+#6) Save Hotel File
+#7) Create New Hotel File
+# You are free to design and use any technology learnt up to now!
+#some advice, write a function for each item, think of what data structure to store the reservations
+#in i.e. list, dictionary and why.
